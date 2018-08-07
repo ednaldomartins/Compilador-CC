c6f166759a06be3bb53ab7e6e34026cba9c65178
Ednaldo <martinsednaldo@outlook.com>
2018-08-07 16:39:47 -0300

    primeira versão do analisador léxico

--- src/business/control/ControllerAnalisadorLexico.java
+++ src/business/control/ControllerAnalisadorLexico.java
@@ -49,7 +49,7 @@ public class ControllerAnalisadorLexico {
                     }         
                 }
                 
-                //or
+                //operador aditivo "or"
                 if ( i+2 <= tamanhoLinha && linha.substring(i, i+2).matches("or"))
                 {
                     int j = i + 1;
@@ -81,29 +81,100 @@ public class ControllerAnalisadorLexico {
                         }
                 }
                 
-                //relacionais
+                
+                //numeros
+                if (i+1 <= tamanhoLinha && Character.isDigit(linha.charAt(i) )  )
+                {
+                    int j = i + 1;
+                    while(   j <= tamanhoLinha && (linha.substring(i, j).matches( Lexico.NUMEROS_INTEIROS ) || linha.substring(i, j).matches( Lexico.NUMEROS_REAIS ) )  ) 
+                    {j++;}
+                    if (linha.substring(i, j-1).matches( Lexico.NUMEROS_INTEIROS ))
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, j-1), "NUMERO_INTEIRO", l+1 ) );
+                        i = j;
+                    }
+                    else if (linha.substring(i, j-1).matches( Lexico.NUMEROS_REAIS ))
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, j-1), "NUMERO_REAL", l+1 ) );
+                        i = j;
+                    }
+                }
+                
+                //operadores relacionais
                 if ( i+1 <= tamanhoLinha && linha.substring(i, i+1).matches( Lexico.OPERADORES_RELACIONAIS ) )
                 {
-                    System.out.println("DEU certo RELACIONAL");
-                    tabela.add( new Simbolo ( linha.substring(i, i+1), "OPERADOR_RELACIONAL", l+1 ) );
+                    if ( i+2 <= tamanhoLinha && linha.substring(i, i+2).matches( Lexico.OPERADORES_RELACIONAIS ) )
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, i+2), "OPERADOR_RELACIONAL", l+1 ) );
+                        i += 2;
+                    }
+                    else if ( !linha.substring(i, i+2).matches( Lexico.ATRIBUICAO ) )
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, i+1), "OPERADOR_RELACIONAL", l+1 ) );
+                    }
+                    i++;
+                    
                 }
                        
-                //aditivos
+                //operadores aditivos
                 if ( i+1 <= tamanhoLinha && linha.substring(i, i+1).matches( Lexico.OPERADORES_ADITIVOS ) )
                 {
                     tabela.add( new Simbolo ( linha.substring(i, i+1), "OPERADOR_ADITIVO", l+1 ) );
-                    System.out.println("DEU certo +-");
+                    i++;
                 }
-            
-            
+                
+                //atribuição
+                if (   i+1 <= tamanhoLinha && linha.substring(i, i+1).matches( ":" ))
+                {
+                    if ( i+2 <= tamanhoLinha && linha.substring(i, i+2).matches( Lexico.ATRIBUICAO ) )
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, i+2), "ATRIBUICAO", l+1 ) );
+                        i += 2;
+                    }
+                }
+                
+                //delimitadores
+                if ( i+1 <= tamanhoLinha && linha.substring(i, i+1).matches( Lexico.DELIMITADORES ) )
+                {
+                    if ( i+2 <= tamanhoLinha && ( linha.substring(i, i+2).matches( Lexico.ATRIBUICAO ) || linha.substring(i, i+2).matches( Lexico.NUMEROS_INTEIROS )) )
+                    {
+                    }
+                    else 
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, i+1), "DELIMITADOR", l+1 ) );
+                        i++;
+                    }
+                }
+                
+                //operadores multiplicativos
+                if (   i+1 <= tamanhoLinha && ( linha.substring(i, i+1).matches( Lexico.OPERADORES_MULTIPLICATIVOS ))  )
+                {
+                    tabela.add( new Simbolo ( linha.substring(i, i+1), "OPERADOR_MULTIPLICATIVO", l+1 ) );
+                    i++;
+                }
+                
+                //operador multiplicativo and
+                if ( i+3 <= tamanhoLinha && linha.substring(i, i+3).matches("and"))
+                {
+                    int j = i + 1;
+                    while(    j < tamanhoLinha && Character.isLetterOrDigit(linha.charAt(j))   ) {j++;}
+                    if ( j <= tamanhoLinha && linha.substring(i, j).matches( Lexico.OPERADOR_MULTIPLICATIVO_AND ) )
+                    {
+                        tabela.add( new Simbolo ( linha.substring(i, j), "OPERADOR_MULTIPLICATIVO", l+1 ) );
+                        i = j;
+                    }
+                }
+                
             }//fim do for do for de tamanho da linha         
-        }// fim do for de numero de linhas     
+        }// fim do for de numero de linhas   
+        String teste="";
     }//fim do metodo
 
     public void exibirTabela ()
     {
         
     }
+    
     
     /**************************************************************************
     *   Metodo para retornar Arquivo de codigo em List                        *                                                           *  
