805f9dc74b9276d5a7b8afc74bbe27d7c9ec7236
ednaldo <ednaldomartins>
2019-02-08 17:31:20 -0300

    declarações de variáveis em construção.

--- src/main/kotlin/business/control/ControllerAnalisadorSintatico.kt
+++ src/main/kotlin/business/control/ControllerAnalisadorSintatico.kt
@@ -1,5 +1,6 @@
 package business.control
 
+import business.model.Lexico
 import business.model.Simbolo
 import java.util.LinkedList
 
@@ -8,6 +9,9 @@ class ControllerAnalisadorSintatico
     var i: Int = 0  //indice
     lateinit var tab: LinkedList<Simbolo>
 
+    private val VAR = "integer|INTEGER|real|REAL|boolean|BOOLEAN|char|CHAR"
+    private val PROGRAM = "PROGRAM|program"
+
     fun analisar(tabela: LinkedList<Simbolo>)
     {
         tab = tabela    //referencia de tabela
@@ -15,15 +19,15 @@ class ControllerAnalisadorSintatico
     }
 
     /**
-     * Estou incrementando o indice na mesma linha que eu uso o get()
+     * Estou incrementando o indice na mesma linha que eu uso o get() no if
      * o incremento usado é o i++, que deve ser incrementado após o uso da variável i
      * caso encontre algum erro no código, deve-se decrementar o indice para pegar a linha do simbolo que esta errado
      */
     private fun programId()
     {
-        if(tab.get(i++).token == "program")
-            if(tab.get(i++).classificacao == "IDENTIFICADOR")
-                if (tab.get(i++).token == ";")
+        if(tab.get(i++).token.matches(PROGRAM.toRegex()))
+            if(tab.get(i++).classificacao.equals("IDENTIFICADOR"))
+                if (tab.get(i++).token.equals(";"))
                     this.declaracoesVariaveis()
                 else print("ERRO: é esperado um ';' na linha ${tab.get(--i).linha}")
             else print("ERRO: é esperado um 'IDENTIFICADOR' para o program, na linha ${tab.get(--i).linha} ")
@@ -32,8 +36,44 @@ class ControllerAnalisadorSintatico
 
     private fun declaracoesVariaveis()
     {
+        while(tab.get(i).classificacao.equals("COMENTARIO")) {i++}
+        if(tab.get(i).token.equals("var"))
+        {
+            i++
+            listaDeclaracoesVariaveis()
+        }
+        else{/*não há lista de declarações de variaveis. CONTINUE...*/}
+    }
 
+    private fun listaDeclaracoesVariaveis()
+    {
+        while(tab.get(i).classificacao.equals("COMENTARIO")) {i++}
+        if (tab.get(i).classificacao.equals("IDENTIFICADOR"))
+        {
+            //if -> listaDeIdentificadores()  verificar se identificador ja foi declarado para continuar
+            i++
+            if (tab.get(i).token.equals(","))
+            {
+                i++
+                listaDeclaracoesVariaveis()
+            }
+            else if (tab.get(i).token.equals(":"))
+            {
+                i++
+                if (tab.get(i).token.matches(VAR.toRegex()))
+                {
+                    tipo()
+                }
+            }
+            else print("ERRO: é esperado uma 'ATRIBUIÇÃO' ou ',' após um 'IDENTIFICADOR'.")
+        }
+        else print("ERRO: é esperado pelo menos 1 'IDENTIFICADOR' no escopo da lista de variáveis.")
+    }
+
+    private fun tipo()
+    {
 
     }
 
 }
+
