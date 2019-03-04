e288a73931906480fcdad6cbddaabd00271a6474
ednaldomartins <martinsednaldo@outlook.com>
2019-03-04 13:06:51 -0300

    Métodos de empilhar e buscar procedimento foram implementados.

--- src/main/kotlin/business/model/AnalisadorSemantico.kt
+++ src/main/kotlin/business/model/AnalisadorSemantico.kt
@@ -14,20 +14,20 @@ class AnalisadorSemantico
          * código é o 'END', logo quando (profundidadeEscopo > 0) então estamos em uma parte do código onde variáveis ou
          * procedimentos não podem ser declarados.
          */
-        var listaIdentificadores: LinkedList<Identificador> = LinkedList()
-        var profundidadeEscopo:Int = 0
+        private var listaIdentificadores: LinkedList<Identificador> = LinkedList()
+        private var profundidadeEscopo:Int = 0
 
         /**
          *
          */
-        @JvmStatic fun analisaIdentificador():Boolean = if (profundidadeEscopo > 0) buscaIdentificador() else empilhaIdentificador()
+        @JvmStatic fun analisaIdentificador(identificador: Identificador):Boolean = if (profundidadeEscopo > 0) buscaIdentificador(identificador) else empilhaIdentificador(identificador)
 
-        private fun buscaIdentificador(): Boolean
+        private fun buscaIdentificador(identificador: Identificador): Boolean
         {
             return true
         }
 
-        private fun empilhaIdentificador(): Boolean
+        private fun empilhaIdentificador(identificador: Identificador): Boolean
         {
             return true
         }
@@ -41,22 +41,70 @@ class AnalisadorSemantico
         /**
          *
          */
-        @JvmStatic fun analisaProcedimento():Boolean = if (profundidadeEscopo > 0) buscaProcedimento() else empilharProcedimento()
+        @JvmStatic fun analisaProcedimento(identificador: Identificador):Boolean = if (profundidadeEscopo > 0) buscaProcedimento(identificador) else empilhaProcedimento(identificador)
 
-        private fun buscaProcedimento(): Boolean
+        private fun buscaProcedimento(identificador: Identificador): Boolean
         {
-            return true
+            //A leitura da pilha começa de cima para baixo, logo os últimos identificadores são os primeiros.
+            var i:Int = 0
+            while (listaIdentificadores.get(i).nome != "#")
+            {
+                if (listaIdentificadores.get(i).nome.equals(identificador.nome) && listaIdentificadores.get(i).tipo.equals((identificador.tipo)))
+                    return true
+                i++
+            }
+            return false
         }
 
-        private fun empilharProcedimento(): Boolean
+        private fun empilhaProcedimento(identificador: Identificador): Boolean
         {
-            return true
+            if (identificador.tipo.equals("procedure"))
+            {
+                //se já existe procedimento ou programa com o mesmo nome então não pode adicionar a pilha. retorna erro
+                if (buscaProcedimento(identificador))
+                {
+                    println("ERRO SEMÂNTICO: Esse nome já foi usado como identificador de um outro procedimento nesse mesmo escopo")
+                    return false
+                }
+                //verificar se o nome do procedimento é igual ao nome do programa
+                if (identificador.nome.equals(listaIdentificadores.get(listaIdentificadores.size-2).nome))
+                {
+                    println("ERRO SEMÂNTICO: Esse nome já foi usado como identificador no nome do programa")
+                    return false
+                }
+                listaIdentificadores.push(identificador)
+                listaIdentificadores.push(Identificador("#", "#"))
+                return true
+            }
+            else if (identificador.tipo.equals("program"))
+            {
+                listaIdentificadores.push(Identificador("#", "#"))
+                listaIdentificadores.push(identificador)
+                return true
+            }
+            else
+            {
+                println("ERRO SEMÂNTICO: Erro inesperado. Problema no tipo do procedimento ou programa. Como isso chegou aqui???!!!")
+                return false
+            }
         }
 
         private fun desempilhaProcedimento(): Boolean
         {
             return true
         }
+
+        /***
+         * Métodos auxiliares
+         */
+        @JvmStatic fun definirTipoDasVariaveis (tipo:String)
+        {
+            var i = listaIdentificadores.size - 1
+            while (i > 0 && listaIdentificadores.get(i).tipo.equals(""))
+                listaIdentificadores.get(i--).tipo = tipo
+        }
+
+        @JvmStatic fun pilhaVazia(): Boolean = this.listaIdentificadores.isNullOrEmpty()
 
     }
 
